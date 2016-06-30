54b21d4626f9b57d5ed0564f3671c5f48f88f0fc
Christophe Gosiau <christophe.gosiau@tigron.be>
2016-06-30 13:37:08 +0200

    first commit

---
+++ lib/Skeleton/Test/Unit.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Skeleton\Test\Unit class
+ *
+ * @author Christophe Gosiau <christophe@tigron.be>
+ * @author Gerry Demaret <gerry@tigron.be>
+ */
+
+namespace Skeleton\Test;
+
+class Unit extends \PHPUnit_Extensions_Selenium2TestCase {
+
+	public static $browsers = [];
+
+
+	public function run(PHPUnit_Framework_TestResult $result = null) {
+		self::$browsers = [
+		    [
+		        'browserName' => 'firefox',
+		        'host' => '10.1.0.52',
+		        'port' => 4444,
+		    ],
+		    [
+		        'browserName' => 'chrome',
+		        'host' => '10.1.0.52',
+		        'port' => 4444,
+		    ],
+		];
+
+		parent::run($result);
+	}
+
+}
