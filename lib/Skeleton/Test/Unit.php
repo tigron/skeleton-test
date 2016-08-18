8ddaf7d055faf899f0d5e4d2a5ec896d393ed8d6
Gerry Demaret <gerry@tigron.be>
2016-08-18 17:16:54 +0200

    Close the webdriver connection after the last test
    Previously, we waited for the object to be destructed before closing
    the webdriver connection. As the object only gets destructed at the
    end of the full test run, this meant that each scene caused one
    connection to remain open, until we hit the limit of the hub, at
    which point the test failed.

--- lib/Skeleton/Test/Unit.php
+++ lib/Skeleton/Test/Unit.php
@@ -19,25 +19,49 @@ class Unit extends \PHPUnit_Framework_TestCase {
 	 * @access public
 	 * @var Facebook\WebDriver\Remote\RemoteWebDriver $webdriver
 	 */
-	public $webdriver = null;
+	private static $my_webdriver = null;
 
 	/**
-	 * Create a new testcase
+	 * Catch calls to the "webdriver" property and proxy them to our
+	 * get_webdriver() method.
 	 *
 	 * @access public
+	 * @param string $key
+	 * @return mixed
 	 */
-	public function __construct() {
-		$capabilities = DesiredCapabilities::chrome();
-		$driver = RemoteWebDriver::create(Config::$selenium_hub, $capabilities, 5000);
-		$this->webdriver = $driver;
+	public function __get($key) {
+		if ($key === 'webdriver') {
+			return self::get_webdriver();
+		}
 	}
 
 	/**
-	 * Destruct the webdriver
+	 * Initialize a new webdriver connection if needed
 	 *
 	 * @access public
 	 */
-	public function __destruct() {
-		$this->webdriver->quit();
+	public static function get_webdriver() {
+		if (self::$my_webdriver === null) {
+			$capabilities = DesiredCapabilities::chrome();
+			$driver = RemoteWebDriver::create(Config::$selenium_hub, $capabilities);
+			self::$my_webdriver = $driver;
+			self::$my_webdriver->manage()->timeouts()->implicitlyWait(1);
+		}
+
+		return self::$my_webdriver;
+	}
+
+	/**
+	 * This method is called after the last case in the test has been run.
+	 * We can safely terminate the webdriver connection here.
+	 *
+	 * @access public
+	 */
+	public static function tearDownAfterClass() {
+		echo "tearDownAfterClass";
+		if (self::$my_webdriver !== null) {
+			self::$my_webdriver->quit();
+			self::$my_webdriver = null;
+		}
 	}
 }
