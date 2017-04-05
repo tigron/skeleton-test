0466ce3b02e4120e030f5249885819d4a9fceb15
Lionel <lionel.laffineur@gmail.com>
2017-04-05 17:56:38 +0200

    Create class for timing file not found
    Created a class extending exception that will be thrown if a timing file has been configured but doesn't exist.

---
+++ lib/Skeleton/Test/Timingfilenotfound.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Skeleton\Test\Timingfilenotfound class
+ *
+ * @author Lionel Laffineur <lionel@tigron.be>
+ */
+
+namespace Skeleton\Test;
+
+class Timingfilenotfound extends \Exception {
+}
