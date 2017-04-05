ac3ddf10ae8883db7ba4ad82f9089a770873df21
Lionel <lionel.laffineur@gmail.com>
2017-04-05 17:27:19 +0200

    Added two variables for files used in timings
    Added two variables that have to be initialized with file names (& path) of files that will be used to calculate timings in the tests.

--- lib/Skeleton/Test/Config.php
+++ lib/Skeleton/Test/Config.php
@@ -4,6 +4,7 @@
  *
  * @author Christophe Gosiau <christophe@tigron.be>
  * @author Gerry Demaret <gerry@tigron.be>
+ * @author Lionel Laffineur <lionel@tigron.be>
  */
 
 namespace Skeleton\Test;
@@ -28,4 +29,23 @@ class Config {
 	 */
 	public static $test_directory = null;
 
+	/**
+	 * Name of the file where the start timestamp will be stored
+	 *
+	 * This file should be filled in with the timestamp when test starts (i.e.: date +%s > /tmp/skeleton-test-start-time.txt)
+	 *
+	 * @access public
+	 * @var string $start_timestamp_filename
+	 */
+	public static $start_timestamp_filename = null;
+
+	/**
+	 * Name of the file where the timings will be saved
+	 *
+	 * This file will be filled in with the start and stop timings of every test in json format
+	 *
+	 * @access public
+	 * @var string $timings_filename
+	 */
+	public static $timings_filename = null;
 }
