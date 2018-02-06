c4494b1651db7d69896fbcecdb61ffa74028c2ee
Christophe Gosiau <christophe@tigron.be>
2018-02-06 13:04:49 +0100

    bugfix: rollback last change

--- lib/Skeleton/Test/Selenium/Webdriver/Element.php
+++ lib/Skeleton/Test/Selenium/Webdriver/Element.php
@@ -34,10 +34,6 @@ class Element extends \Facebook\WebDriver\Remote\RemoteWebElement {
     		throw new \Exception('The element you try to click is not enabled');
     	}
 
-		$this->selenium_webdriver->wait(10, 2000)->until(
-			WebDriverExpectedCondition::elementToBeClickable($this)
-		);
-
         parent::click();
         $webdriver = $this->selenium_webdriver;
         $this->selenium_webdriver->wait()->until(
